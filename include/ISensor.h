ee2d2f6ad95be79817f810b343fd8c6b394c8c2a
uch.NZ <1@0762.ru>
2022-05-02 01:40:40 +0300

    folders and files cleanup

--- include/ISensor.h
+++ include/ISensor.h
@@ -4,7 +4,6 @@
 class ISensor
 {
 public:
-    // virtual void requestCurrentMeasurement() = 0;
     virtual bool requestCurrentMeasurement() = 0;
     virtual uint8_t getNumberOfConnectedSensors() = 0;
     virtual double getCurrentMeasurementByID(uint8_t id) = 0;
